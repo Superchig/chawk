c2923ce8570359c5c8cb6ae11a8852a55a8a3d5f
Christopher Chang <chiggiechang@gmail.com>
2022-04-27 17:10:54 -0700

    Modify `raw-parser` binary to take an awk source file as a CLI argument

--- src/raw_parser.rs
+++ src/raw_parser.rs
@@ -3,7 +3,8 @@ use pest::Parser;
 use std::fs;
 
 fn main() {
-    let unparsed_file = fs::read_to_string("examples/print_line.awk").expect("Cannot read file");
+    let input_file = std::env::args().nth(1).unwrap();
+    let unparsed_file = fs::read_to_string(&input_file).expect("Cannot read file");
 
     let program = ChawkParser::parse(Rule::Program, &unparsed_file)
         .expect("unsuccessful parse") // unwrap the parse result
