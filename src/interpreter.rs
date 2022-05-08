8a47c758d310110fb25447a39a240a886e9a4dff
Christopher Chang <chiggiechang@gmail.com>
2022-05-07 23:53:45 -0700

    Remove addressed FIXME regarding regex matching with `~`

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -19,7 +19,6 @@ pub struct Interpreter {
 }
 
 impl Interpreter {
-    // FIXME(Chris): Implement string-specific regex matching with ~
     // FIXME(Chris): Implement function definitions and function calls
     pub fn run(&mut self, program_str: &str, records_reader: &mut dyn BufRead) {
         let program_ast = parse(program_str).unwrap();
